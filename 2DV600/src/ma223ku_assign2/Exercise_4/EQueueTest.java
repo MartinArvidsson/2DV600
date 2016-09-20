2040f180f322af89715abcce8cca2c03cb713389
Martin Arvidsson <Martinarvidsson1994@gmail.com>
2016-09-20 21:16:04 +0200

    Done with 4 i believe

--- 2DV600/src/ma223ku_assign2/Exercise_4/EQueueTest.java
+++ 2DV600/src/ma223ku_assign2/Exercise_4/EQueueTest.java
@@ -38,37 +38,87 @@ public class EQueueTest
     @Test
     public void dequeue() throws Exception
     {
+        EQueue _queue = new EQueue();
+        EQueue testqueue = new EQueue();
+        _queue.enqueue(1);
+        _queue.enqueue("Test");
+        _queue.enqueue(testqueue);
 
+        _queue.dequeue();
+        _queue.dequeue();
+        _queue.dequeue();
+
+        try
+        {
+            _queue.dequeue();
+            fail("Test failed, no exception was thrown");
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
     }
 
     @Test
     public void size() throws Exception
     {
         EQueue _queue = new EQueue();
+        for (int i = 0; i < 100; i++)
+        {
+            _queue.enqueue(i);
+        }
+        _queue.size();
+        for (int i = 0; i < 50; i++)
+        {
+            _queue.dequeue();
+        }
+        _queue.size();
     }
 
     @Test
     public void isEmpty() throws Exception
     {
-
+        EQueue _queue = new EQueue();
+        _queue.isEmpty();
+        _queue.enqueue(1);
+        if(!_queue.isEmpty())
+        {
+            assertTrue(true);
+        }
     }
 
     @Test
     public void first() throws Exception
     {
-
+        EQueue _queue = new EQueue();
+        try
+        {
+           _queue.first();
+            fail("Test failed, no exception was thrown");
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
+        _queue.enqueue(1);
+        _queue.first();
     }
 
     @Test
     public void last() throws Exception
     {
+        EQueue _queue = new EQueue();
+        try
+        {
+            _queue.last();
+            fail("Test failed, no exception was thrown");
+        }
+        catch (IndexOutOfBoundsException e)
+        {
+            assertTrue(true);
+        }
+        _queue.enqueue(1);
+        _queue.last();
 
     }
-
-    @Test
-    public void iterator() throws Exception
-    {
-
-    }
-
 }
