25ff1df94b7dda3636732c6b6a74d41cb0085f7a
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2016-09-28 17:29:05 +0200

    Working on 3

--- 2DV600/src/ma223ku_assign3/MyGraph.java
+++ 2DV600/src/ma223ku_assign3/MyGraph.java
@@ -3,57 +3,106 @@ package ma223ku_assign3;
 import graphs.DirectedGraph;
 import graphs.Node;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Martin on 2016-09-27.
  */
-public class MyGraph implements DirectedGraph
+public class MyGraph<E> implements DirectedGraph<E>
 {
-    public MyGraph()
-    {
-
-    }
-
+    private Map<E,MyNode<E>> item2node = new HashMap<E, MyNode<E>>();
+    private Set<Node<E>> heads = new HashSet<>();
+    private Set<Node<E>> tails = new HashSet<>();
     @Override
-    public Node addNodeFor(Object item)
+    public Node addNodeFor(E item)
     {
-        return null;
+        if (containsNodeFor(item))
+        {
+            System.out.println("Item already exist in the hashset");
+            return null;
+        } else
+        {
+            MyNode _mynode = new MyNode<>(item);
+            item2node.put(item, _mynode);
+            return _mynode;
+        }
     }
 
     @Override
     public Node getNodeFor(Object item)
     {
-        return null;
+
+        if(item == null)
+        {
+            throw new RuntimeException("Null as input");
+        }
+        else if (item2node.get(item) != null)
+        {
+            return item2node.get(item);
+        }
+        else if (item2node.get(item) == null)
+        {
+            throw new IndexOutOfBoundsException();
+        }
+        else
+        {
+            return null;
+        }
     }
 
     @Override
-    public boolean addEdgeFor(Object from, Object to)
+    public boolean addEdgeFor(E from, E to)
     {
-        return false;
+        if(from == null || to == null)
+        {
+            throw new RuntimeException("Recieved null as input");
+        }
+
+        MyNode<E> source = (MyNode<E>)addNodeFor(from);
+        MyNode<E> target = (MyNode<E>)addNodeFor(to);
+
+        if(source.hasSucc(target))
+        {
+            return false;
+        }
+        else
+        {
+            source.addSucc(target);
+            target.addPred(source);
+
+            tails.remove(source);
+            heads.remove(target);
+            return true;
+        }
     }
 
     @Override
     public boolean containsNodeFor(Object item)
     {
-        return false;
+        if(item2node.containsKey(item))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     @Override
     public int nodeCount()
     {
-        return 0;
+        return item2node.size();
     }
 
     @Override
-    public Iterator<Node> iterator()
+    public Iterator<Node<E>> iterator()
     {
         return null;
     }
 
     @Override
-    public Iterator<Node> heads()
+    public Iterator<Node<E>> heads()
     {
         return null;
     }
@@ -61,11 +110,11 @@ public class MyGraph implements DirectedGraph
     @Override
     public int headCount()
     {
-        return 0;
+        return heads.size();
     }
 
     @Override
-    public Iterator<Node> tails()
+    public Iterator<Node<E>> tails()
     {
         return null;
     }
@@ -73,7 +122,7 @@ public class MyGraph implements DirectedGraph
     @Override
     public int tailCount()
     {
-        return 0;
+        return tails.size();
     }
 
     @Override
@@ -97,12 +146,22 @@ public class MyGraph implements DirectedGraph
     @Override
     public boolean containsEdgeFor(Object from, Object to)
     {
+        if(from == null || to == null)
+        {
+            throw new RuntimeException("Recieved null as input");
+        }
+
         return false;
     }
 
     @Override
     public boolean removeEdgeFor(Object from, Object to)
     {
+        if(from == null || to == null)
+        {
+            throw new RuntimeException("Recieved null as input");
+        }
+
         return false;
     }
 }
--- 2DV600/src/ma223ku_assign3/MyNode.java
+++ 2DV600/src/ma223ku_assign3/MyNode.java
@@ -1,8 +1,94 @@
 package ma223ku_assign3;
 
+
+import graphs.Node;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by Martin on 2016-09-27.
  */
-public class MyNode
+public class MyNode<E> extends Node<E>
 {
+    private Set<Node<E>> preds = new HashSet<>();
+    private Set<Node<E>> succs = new HashSet<>();
+    public int num;
+
+    /**
+     * Constructs a new node using <tt>item</tt> as key.
+     *
+     * @param item
+     */
+    protected MyNode(E item)
+    {
+        super(item);
+    }
+
+    @Override
+    public boolean hasSucc(Node<E> node)
+    {
+        return false;
+    }
+
+    @Override
+    public int outDegree()
+    {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Node<E>> succsOf()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean hasPred(Node<E> node)
+    {
+        return false;
+    }
+
+    @Override
+    public int inDegree()
+    {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Node<E>> predsOf()
+    {
+        return null;
+    }
+
+    @Override
+    protected void addSucc(Node<E> succ)
+    {
+        succs.add(succ);
+    }
+
+    @Override
+    protected void removeSucc(Node<E> succ)
+    {
+
+    }
+
+    @Override
+    protected void addPred(Node<E> pred)
+    {
+
+    }
+
+    @Override
+    protected void removePred(Node<E> pred)
+    {
+
+    }
+
+    @Override
+    protected void disconnect()
+    {
+
+    }
 }
