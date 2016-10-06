213211d116e7bfe5b0644d8e17ef1454ba8955e9
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2016-10-06 17:27:17 +0200

    Started on ConnectedComponents

--- 2DV600/src/ma223ku_assign3/MyConnectedComponents.java
+++ 2DV600/src/ma223ku_assign3/MyConnectedComponents.java
@@ -4,7 +4,10 @@ import graphs.ConnectedComponents;
 import graphs.DirectedGraph;
 import graphs.Node;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * Created by Martin on 2016-09-30.
@@ -14,6 +17,20 @@ public class MyConnectedComponents<E> implements ConnectedComponents<E>
     @Override
     public Collection<Collection<Node<E>>> computeComponents(DirectedGraph<E> dg)
     {
+        Collection<Node<E>> components = new ArrayList<>();
+        Iterator<Node<E>> iterator = dg.iterator();
+
+        while(iterator.hasNext())
+        {
+            components.add(iterator.next());
+        }
+        for(Node<E> item : components)
+        {
+            if(item.succsOf() != item.predsOf())
+            {
+
+            }
+        }
         return null;
     }
 }
--- 2DV600/src/ma223ku_assign3/MyTransitiveClosure.java
+++ 2DV600/src/ma223ku_assign3/MyTransitiveClosure.java
@@ -4,8 +4,7 @@ import graphs.DirectedGraph;
 import graphs.Node;
 import graphs.TransitiveClosure;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Martin on 2016-09-30.
@@ -15,6 +14,17 @@ public class MyTransitiveClosure<E> implements TransitiveClosure<E>
     @Override
     public Map<Node<E>, Collection<Node<E>>> computeClosure(DirectedGraph<E> dg)
     {
-        return null;
+        Map<Node<E>,Collection<Node<E>>> nodemap = new HashMap<>();
+        MyDFS dfs = new MyDFS();
+
+        Iterator<Node<E>> iterator = dg.iterator();
+
+        while(iterator.hasNext())
+        {
+            Node<E> nodeitem = iterator.next();
+            List i = dfs.dfs(dg,nodeitem);
+            nodemap.put(nodeitem,i);
+        }
+        return nodemap;
     }
 }
